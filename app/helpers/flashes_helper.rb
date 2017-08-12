5908fe6791ea2d0940408658a82b698c0ab64c7e
leunginn <leungwan2016@gmail.com>
2017-08-12 19:52:00 +0800

    add bootstrap flash function-fix footer,flashes_helper

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,9 +1,9 @@
-module FlashesHelpers
+module FlashesHelper
   FLASH_CLASSES={alert:"danger",notice:"success",warning:"warning"}.freeze
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym,key
   end
   def user_facing_flashes
-    flash.to_bash.slice "alert","notice","warning"
+    flash.to_hash.slice "alert","notice","warning"
   end
 end
